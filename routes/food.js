75ec9d1ec3dda822d3a8e6d1ebea8a59b571078f
cmlinac <cmlinac@gmail.com>
2020-06-07 22:53:38 -0500

    review how query parameters work in express and implement em

--- routes/food.js
+++ routes/food.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET a specific food. */
 router.get("/:food", function (req, res, next) {
-  res.send("GET a food");
+  res.send("GET a food " + req.params.food);
 });
 
 router.get("/search/:queries", function (req, res, next) {
-  res.send("search a food");
+  res.send("search a food " + req.params / queries);
 });
 
 module.exports = router;
--- routes/log.js
+++ routes/log.js
@@ -8,22 +8,22 @@ router.get('/user/:user', function(req, res, next) {
 
 // GET a specific log
 router.get('/:log', function(req, res, next) {
-  res.send('Log is ' + log);
+  res.send('Log is ' + req.params.log);
 });
 
 // add a log
 router.post('/:user', function(req, res, next) {
-  res.send('Add log for user ' + user);
+  res.send('Add log for user ' + req.params.user);
 });
 
 // update a log
 router.put('/:log', function(req, res, next) {
-  res.send('Update log ' + log);
+  res.send('Update log ' + req.params.log);
 });
 
 // delete a log
 router.delete('/:log', function(req, res, next) {
-  res.send('Delete log ' + log);
+  res.send('Delete log ' + req.params.log);
 });
 
 module.exports = router;
